d5e362c7299038473c40efa0ad6a3f200169ed98
mohamed.tahiri <mhdtahiri01@gmail.com>
2025-12-17 10:55:34 +0100

    test fail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
 
   // --- Lazy loaded modules ---
-  { path: 'dev', loadChildren: () => import('./features/dev/dev.module').then(m => m.DevModule) },
+  { path: 'dev', loadChildren: () => import('./features/dev/dev.module').then(m => m.DevModule) }, }}}}}}}}}
   { path: 'shop', loadChildren: () => import('./features/shop/shop.module').then(m => m.ShopModule) },
   { path: 'account', loadChildren: () => import('./features/account/account.module').then(m => m.AccountModule) },
   { path: 'admin', loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule) },
